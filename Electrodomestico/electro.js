841293939450bade550693f8046d442b1c6b0a4d
João Paulino <joaopaulino399@gmail.com>
2023-10-31 21:04:58 +0100

    update electro.js

--- Electrodomestico/electro.js
+++ Electrodomestico/electro.js
@@ -6,4 +6,18 @@ const product = [
         price: 120,
         category: 'mobile'
     },
+    {
+        id: 1,
+        image: 'pngwing.com(2).png',
+        title: 'Z Flip Foldable Mobile',
+        price: 120,
+        category: 'mobile'
+    },
+    {
+        id: 1,
+        image: 'pngwing.com(3).png',
+        title: 'Z Flip Foldable Mobile',
+        price: 120,
+        category: 'mobile'
+    },
 ]
