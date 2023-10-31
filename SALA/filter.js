51e6146463b3e532f8a437648fef967113489d27
João Paulino <joaopaulino399@gmail.com>
2023-10-31 08:25:47 +0100

    update filter.js

--- SALA/filter.js
+++ SALA/filter.js
@@ -1,32 +1,32 @@
-constbtns=[
-{
-    id: 1,
-    name: 'Mobiles'
-},
-{
-    id: 2,
-    name:'Watches'
-},
-{
-    id: 3,
-    name: 'Cameras'
-},
-{
-    id: 4,
-    name: 'Laptops'
-},
-{
-    id: 5,
-    name: 'Air Pods'
-},
-]
-
-const filters = [...new Set(btns.map((btn)=>
-            {return btn}))]
-
-document.getElementById('btns').innerHTML=filters.map((btn)={
-    var {name, id} = btn;
-    return(
-        "<button class='fil-p' onclick='filterItems("+(id)+`)'>${name}</button>`
-    )
-}).join('');
+const btns = [
+    {
+      id: 1,
+      name: 'Mobiles'
+    },
+    {
+      id: 2,
+      name: 'Watches'
+    },
+    {
+      id: 3,
+      name: 'Cameras'
+    },
+    {
+      id: 4,
+      name: 'Laptops'
+    },
+    {
+      id: 5,
+      name: 'Air Pods'
+    }
+  ];
+  
+  const filters = [...new Set(btns.map((btn) => btn.id))];
+  
+  document.getElementById('btns').innerHTML = filters
+    .map((id) => {
+      const btn = btns.find((btn) => btn.id === id);
+      return `<button class='fil-p' onclick='filterItems(${btn.id})'>${btn.name}</button>`;
+    })
+    .join('');
+  
