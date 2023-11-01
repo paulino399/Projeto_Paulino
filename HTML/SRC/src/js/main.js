ea47e379a4a4b903c604a78465f0f2bf428dafff
João Paulino <joaopaulino399@gmail.com>
2023-11-01 20:42:42 +0100

    feat: create main.js

---
+++ HTML/SRC/src/js/main.js
@@ -0,0 +1,14 @@
+/* 
+let xhr = new XMLHttpRequest();
+xhr.open('get', '/src/data/bd.json');
+
+
+xhr.setRequestHeader("content-type", "application/json");
+xhr.onload = ()=>{
+    console.log(xhr.responseText);
+}
+
+xhr.send() *//* 
+let fs = require('fs');
+
+console.log(fs.readdir('/')) */
