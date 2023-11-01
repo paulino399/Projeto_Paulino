faedcf3ad9257bf94431516ceb1e831450b340d6
João Paulino <joaopaulino399@gmail.com>
2023-11-01 11:16:55 +0100

    update pagamento.js

--- Pagamentos/pagamento.js
+++ Pagamentos/pagamento.js
@@ -1,2 +1,4 @@
 let tColorA = document.getElementById('tColorA'),
 tColorB = document.getElementById('tColorB'),
+tColorC = document.getElementById('tColorC'),
+iconA = document.querySelector('.fa-credit-card'),
