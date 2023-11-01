45c0ceb26563c2b5f7eecc5b7c32eac19617b507
João Paulino <joaopaulino399@gmail.com>
2023-11-01 11:43:21 +0100

    update pagamento.js

--- Pagamentos/pagamento.js
+++ Pagamentos/pagamento.js
@@ -47,7 +47,19 @@ cNumber.addEventListener('keyup', function(e){
     for( var i = 0; i < num.length; i++) {
         if(i%4 ==0 && i>0) newValue = newValue.concat(' ');
         newValue = newValue.concat(num[i]);
-        cNumber
+        cNumber.value = newValue;
+    }
+
+    let ccNum = document.getElementById('c-number');
+cNumber.addEventListener('keyup', function(e){
+    let num = cNumber.value;
+
+    let newValue = '';
+    num = num.replace(/\s/g, '');
+    for( var i = 0; i < num.length; i++) {
+        if(i%4 ==0 && i>0) newValue = newValue.concat(' ');
+        newValue = newValue.concat(num[i]);
+        cNumber.value = newValue;
     }
 
 
