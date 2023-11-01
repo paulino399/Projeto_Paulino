c7097cd2ca9ff44799905768c80ebe3d929f895c
João Paulino <joaopaulino399@gmail.com>
2023-11-01 11:19:15 +0100

    update pagamento.js

--- Pagamentos/pagamento.js
+++ Pagamentos/pagamento.js
@@ -3,3 +3,5 @@ tColorB = document.getElementById('tColorB'),
 tColorC = document.getElementById('tColorC'),
 iconA = document.querySelector('.fa-credit-card'),
 iconB = document.querySelector('.fa-landmark'),
+iconC = document.querySelector('.fa-wallet'),
+
